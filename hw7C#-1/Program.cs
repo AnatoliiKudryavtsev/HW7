9cfff59202d4f0f0bb2af7f28e9876acf6f77331
unknown <Anatolii>
2022-12-16 16:19:52 +0300

    add folders

---
+++ hw7C#-1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Необходимо ввести количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+double[,] arrayDouble = GetArrayDouble(rows, columns, 0, 10); 
+PrintArrayDouble(arrayDouble);
+
+double[,] GetArrayDouble(int m, int n, int min, int max) 
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(max-min);
+        }
+    }
+    return result;
+}
+
+void PrintArrayDouble(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i,j]:f2} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ hw7C#-2/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, и возвращает значение 
+//  этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число для проверки наличия данного числа в массиве: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns,0,3); 
+PrintArray(array); 
+
+if(FindNumber(array,number)) 
+{
+    Console.WriteLine( $"Число найдено {number}");
+}
+else
+{
+    Console.WriteLine("Введенного числа в массиве нет");
+}
+
+int[,] GetArray(int m, int n, int min, int max) 
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray) 
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+bool FindNumber(int[,] array, int num) 
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j]==num) return true; 
+        }
+    }
+    return false;
+}
